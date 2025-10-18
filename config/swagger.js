b7caeadbf7e3b7ca0ea264066ed8fec742e12465
Nguyen Huy Hoang <137654109+huyhoang8704@users.noreply.github.com>
2025-10-18 11:51:35 +0700

    fix swagger

--- config/swagger.js
+++ config/swagger.js
@@ -4,7 +4,7 @@ const swaggerUi = require("swagger-ui-express");
 require("dotenv").config();
 
 const PORT = process.env.PORT || 4000;
-const BASE_URL = "https://electronic-commerce-co-3027-89i11726s.vercel.app";
+const BASE_URL = "https://electronic-commerce-co-3027.vercel.app";
 
 const options = {
   definition: {
