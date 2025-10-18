e2565eb6d47af83c3a133ad3d4c5e759eca1dc68
Nguyen Huy Hoang <137654109+huyhoang8704@users.noreply.github.com>
2025-10-18 11:49:56 +0700

    fix swagger

--- config/swagger.js
+++ config/swagger.js
@@ -4,6 +4,7 @@ const swaggerUi = require("swagger-ui-express");
 require("dotenv").config();
 
 const PORT = process.env.PORT || 4000;
+const BASE_URL = "https://electronic-commerce-co-3027-89i11726s.vercel.app";
 
 const options = {
   definition: {
@@ -15,8 +16,12 @@ const options = {
     },
     servers: [
       {
+        url: BASE_URL,
+        description: "Production server (Vercel)",
+      },
+      {
         url: `http://localhost:${PORT}`,
-        description: "Development server",
+        description: "Local development server",
       },
     ],
     components: {
@@ -36,7 +41,11 @@ const options = {
 const swaggerSpec = swaggerJsdoc(options);
 
 function swaggerDocs(app) {
-  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+  app.use(
+    "/api-docs",
+    swaggerUi.serveFiles(swaggerSpec, {}),
+    swaggerUi.setup(swaggerSpec)
+  );
 }
 
 module.exports = swaggerDocs;
